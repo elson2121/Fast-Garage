9bdffb6e92949c005f23e5a08a1a3ca25c85accd
elson2121 <elsonsd21@gmail.com>
2025-12-14 08:57:16 -0300

    set up the mysql2 file

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,32 @@
 //import express module
 const express = require('express');
+//import mysql2 module
+const mysql = require('mysql2');
 // set up the listener port 
 const app=express();
 //create an listener port 
 const port = 4000;
+//define database connection parameters
+const dbConfig = {
+    host: 'localhost',
+    user: 'garage',
+    password:'garage',
+    database: 'garage',
+
+};
+//create a connection to the database
+const connection = mysql.createConnection(dbConfig);
+//connect to the database
+connection.connect((err)=>{
+    if(err){
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+});
+
+
+
 //create the simple get request handler 
 app.get('/',(req,res)=>{
     res.send('Hello World!');
