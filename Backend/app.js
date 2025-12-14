0485f5c2bd8a3fbaa9a6943c5439cf3ec3a7c2c3
elson2121 <elsonsd21@gmail.com>
2025-12-14 08:31:26 -0300

    update the app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,15 @@
 //import express module
 const express = require('express');
-//create an express applicatoin
-appm
+// set up the listener port 
+const app=express();
+//create an listener port 
+const port = 4000;
+//create the simple get request handler 
+app.get('/',(req,res)=>{
+    res.send('Hello World!');
+
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on port http://localhost:${port}`);
+});
