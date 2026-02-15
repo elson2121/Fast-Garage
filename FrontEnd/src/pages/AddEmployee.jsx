eb6d850cde129e467abf27f98a83ae8956588a62
elson2121 <elsonsd21@gmail.com>
2026-02-15 11:20:12 -0800

    add the form for the the add employee

--- FrontEnd/src/pages/AddEmployee.jsx
+++ FrontEnd/src/pages/AddEmployee.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 
 function AddEmployee() {
   return (
-    <div>
-      hello this is the emplyoy 
-    </div>
+    <>
+     <h4> Add the Employee </h4>
+     <div>
+      <form action="">
+      <label htmlFor="fname">First Name:</label>
+      <input type="text" id="fname" name="fname" />
+      <br />
+      <label htmlFor="lname">Last Name:</label>
+      <input type="text" id="lname" name="lname" />
+      <br />
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" />
+      <br />
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" name="password" />
+      <br />
+      <input type="submit" value="Submit" />
+
+
+
+
+      </form>
+     </div>
+
+
+    </>
   )
 }
 
