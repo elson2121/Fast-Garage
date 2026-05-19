3df20686dd5ca6d81c307eb8751d7fdef03e5dd0
elson2121 <elsonsd21@gmail.com>
2026-05-19 22:30:14 +0300

    update the set up

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 
-function () {
+export default function Home() {
   return (
     <div>
-      this is the home 
+      //write the register for for useing the name,sex, department
+      <h1> Home </h1>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" required />
+        <br />  
+        <label htmlFor="sex">Sex:</label>
+        <input type="text" id="sex" name="sex" required />
+        <br />
+        <label htmlFor="department">Department:</label>
+        <input type="text" id="department" name="department" required />
+        <br />
+        <button type="submit">Register</button>
+      </form>
+      // add the crude operations for the 
+
+
+
     </div>
   )
 }
-
-export default 
